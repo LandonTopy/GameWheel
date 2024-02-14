01151ce9646a5bd2d732aecf0c5d2a8aee671a88
LandonTopy <landontopy@gmail.com>
2024-02-14 09:32:44 -0500

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+public class GameClass {
+    Scanner scan = new Scanner(System.in);
+    public static void play(GameWheel g){
+
+    }
+
+}
